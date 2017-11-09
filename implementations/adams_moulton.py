3094f3e466856de4dc46df91e343c99658582b66
Nathan Prestwood <nmf2@cin.ufpe.br>
2017-11-09 15:42:22 -0300

    Implementing Adams Moulton

--- implementations/adams_moulton.py
+++ implementations/adams_moulton.py
@@ -1,13 +1,19 @@
 from sympy import Point2D
+from sympy import solvers
+from sympy import Eq
+from sympy.abc import y as s_y
 
 def method(f, p, h, n, order):
-    print "Running Adams-Bashforth of order {} for {} iterations".format(order, n)
+    print "Running Adams-Moulton of order {} for {} iterations".format(order, n)
         
     points = []
     y = 0
 
-    points.insert(0,"{0} {1}\n".format(float(p[-1].x), float(p[-1].y)))  # save point as a string with the following format: "t y\n"
-        
+    # setup first point
+
+    points.insert(0,"{0} {1}\n".format(float(p[-1].x), float(p[-1].y)))     # save point as a string with the following format: "t y\n"
+    p.insert(0,Point2D(p[-1].x + h, s_y))                                   # add point (tn+1, yn+1), where yn+1 is a symbol
+
     if order == 1:
         for i in range(n + order -1):
             fk = calc_fk(f, p)
@@ -23,7 +29,7 @@ def method(f, p, h, n, order):
     if order == 2:
         for i in range(n + order -1):
             fk = calc_fk(f, p)
-            y = p[0].y + (h/2)*( (3)*fk[0] - fk[1] )
+            y = p[0].y + (h/2)*( fk[0] + fk[1] )
             
             p.insert(0, Point2D(float(p[0].x + h), float(y)))           # inserts the point that was just found into the beginning of the list
             p.pop(-1)                                                   # removes the last item of the list
@@ -32,9 +38,9 @@ def method(f, p, h, n, order):
         return points
         
     if order == 3:
-        for i in range(n + order-1):
+        for i in range(n + order - 1):
             fk = calc_fk(f, p)
-            y = p[0].y + (h/12)*( (23)*fk[0] - (16)*fk[1] + (5)*fk[2] )
+            y = p[0].y + (h/12)*( (5)*fk[0] + (8)*fk[1] - (1)*fk[2] )
             
             p.insert(0, Point2D(float(p[0].x + h), float(y)))           # inserts the point that was just found into the beginning of the list
             p.pop(-1)                                                   # removes the last item of the list
@@ -43,21 +49,27 @@ def method(f, p, h, n, order):
         return points
         
     if order == 4:
-        for i in range(n + order-1):
+        for i in range(n + order - 1):
             fk = calc_fk(f, p)
+            print '\nfk: ',fk
+            print 'p: ', p, '\n'
 
-            y = p[0].y + (h/24)*( (55)*fk[0] - (59)*fk[1] + (37)*fk[2] - (9)*fk[3])
-        
-            p.insert(0, Point2D(float(p[0].x + h), float(y)))           # inserts the point that was just found into the beginning of the list
+            y = solvers.solve(Eq(s_y, p[1].y + (h/24)*( (9)*fk[0] + (19)*fk[1] - (5)*fk[2] + (1)*fk[3]))
+
+            p[0].y = y[0]
+
+            print 'p: ', p, '\n'
+                  
+            p.insert(0, Point2D(float(p[0].x + h), s_y))           # inserts the point that was just found into the beginning of the list
             p.pop(-1)                                                   # removes the last item of the list fn
             
             points.append( "{0} {1}\n".format( float(p[-1].x), float(p[-1].y) ) )
         return points
 
     if order == 5:
-        for i in range(n + order-1):
+        for i in range(n + order - 1):
             fk = calc_fk(f, p)
-            y = p[0].y + (h/720)*( (1901)*fk[0] - (2774)*fk[1] + (2616)*fk[2] - (1274)*fk[3] + (251)*fk[4])
+            y = p[0].y + (h/720)*( (251)*fk[0] + (646)*fk[1] - (264)*fk[2] + (106)*fk[3] - (19)*fk[4])
             
             p.insert(0, Point2D(float(p[0].x + h), float(y)))           # inserts the point that was just found into the beginning of the list
             p.pop(-1)                                                   # removes the last item of the list
@@ -68,7 +80,8 @@ def method(f, p, h, n, order):
     if order == 6:
         for i in range(n + order - 1):
             fk = calc_fk(f, p)
-            y = p[0].y + (h/1440)*( (4277)*fk[0] - (7923)*fk[1] + (9982)*fk[2] - (7298)*fk[3] + (2877)*fk[4] - (475)*fk[5])
+
+            y = p[0].y + (h/1440)*( (475)*fk[0] + (1427)*fk[1] - (798)*fk[2] + (482)*fk[3] - (173)*fk[4] + (27)*fk[5])
 
             p.insert(0, Point2D(float(p[0].x + h), float(y)))           # inserts the point that was just found into the beginning of the list
             p.pop(-1)                                                   # removes the last item of the list
@@ -82,10 +95,9 @@ def calc_fk(f, p):
     #print 'entered calc_fk'
     fk = []
     for point in p:
-        fk.append(float(f.subs('t', point.x).subs('y', point.y)))   # calculate each fk
+        fk.append(f.subs('t', point.x).subs('y', point.y))   # calculate each fk
         
     # fk[0] = fn, fk[1] = fn-1... fk[j] = fn-j
     #print 'fk: ',fk
-        
         
     return fk
--- utils.py
+++ utils.py
@@ -2,141 +2,157 @@ import sympy as sp
 import os
 import subprocess
 from implementations import *
+import implementations
 
 from sympy import Point2D
 
 def getFunction():
-	while True:
-		string = ''
-		try:
-			string = raw_input("Type in the f(t,y) function: ")  # get expression as string
-			expr = sp.sympify(string)  # turn string expression into Sympy expression
-			break
-		except:
-			print "Please try again, invalid input: ", string
-	
-	return expr
+    while True:
+        string = ''
+        try:
+            string = raw_input("Type in the f(t,y) function: ")  # get expression as string
+            expr = sp.sympify(string)  # turn string expression into Sympy expression
+            break
+        except:
+            print "Please try again, invalid input: ", string
+    return expr
 
 
 def getPoint(k):
-	if k is None:  # in case the user doesn't want to specify which point they're getting
-		k = ''
-	
-	while True:
-		try:
-			t = input("Type in t{0}: ".format(k))  # get k-th t as a number
-			break
-		except:
-			print "Please try again, invalid input"
-	
-	while True:
-		try:
-			y = input("Type in y{0}: ".format(k))  # get k-th y as a number
-			break
-		except:
-			print "Please try again, invalid input"
-	
-	return t, y
+    if k is None:  # in case the user doesn't want to specify which point they're getting
+        k = ''
+    while True:
+        try:
+            t = input("Type in t{0}: ".format(k))  # get k-th t as a number
+            break
+        except:
+            print "Please try again, invalid input"
+        
+    while True:
+        try:
+            y = input("Type in y{0}: ".format(k))  # get k-th y as a number
+            break
+        except:
+            print "Please try again, invalid input"
+        
+    return t, y
 
 
 def getNandH():
-	h = input("Type in the step h: ")  # gets h as number
-	n = input("Type in the number of iterations n: ")  # gets h as number
-	
-	return h, n
+    h = input("Type in the step h: ")  # gets h as number
+    n = input("Type in the number of iterations n: ")  # gets h as number
+        
+    return h, n
 
 
 def plot(method):
-	
-	print "Running shell commands... might take a while.\n"
-	
-	p = subprocess.Popen("cat \"implementations/results/{0}.conf\" | gnuplot".format(method), shell=True)
-	os.waitpid(p.pid, 0)
+        
+    print "Running shell commands... might take a while.\n"
+        
+    p = subprocess.Popen("cat \"implementations/results/{0}.conf\" | gnuplot".format(method), shell=True)
+    os.waitpid(p.pid, 0)
 
-	p = subprocess.Popen("xdg-open \"implementations/results/{0}.png\"".format(method), shell=True)
-	os.waitpid(p.pid, 0)
-	
-	print "Done!"
+    p = subprocess.Popen("xdg-open \"implementations/results/{0}.png\"".format(method), shell=True)
+    os.waitpid(p.pid, 0)
+        
+    print "Done!"
 
 
 def generateGnuPlotConf(pts, method):
-	
-	# ensure directory exists
-	
-	directory = os.path.dirname('implementations/results/')
-	if not os.path.exists(directory):
-		os.makedirs(directory)
+        
+    # ensure directory exists
+        
+    directory = os.path.dirname('implementations/results/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-	# write points to file:
-	f = open('implementations/results/{0}_points.txt'.format(method), 'w+')
-	f.writelines(pts)
-	f.close()
-	
-	# generate gnuplot configuration file:
-	conf = '''\tset terminal png truecolor
-			set output "implementations/results/{0}.png"
-			set autoscale
-			set style data lines
-			plot "implementations/results/{0}_points.txt" using 1:2 title "{0}"'''.format(method)
-	
-	f = open('implementations/results/{0}.conf'.format(method), 'w')
-	f.writelines(conf)
-	f.close()
+    # write points to file:
+    f = open('implementations/results/{0}_points.txt'.format(method), 'w+')
+    f.writelines(pts)
+    f.close()
+        
+    # generate gnuplot configuration file:
+    conf = '''\tset terminal png truecolor
+            set output "implementations/results/{0}.png"
+            set autoscale
+            set style data lines
+            plot "implementations/results/{0}_points.txt" using 1:2 title "{0}"'''.format(method)
+        
+    f = open('implementations/results/{0}.conf'.format(method), 'w')
+    f.writelines(conf)
+    f.close()
 
 def runMethod(method, name):
-	f = getFunction()
+    f = getFunction()
 
-	t, y = getPoint(0)
+    t, y = getPoint(0)
 
-	h, n = getNandH()
+    h, n = getNandH()
 
-	points = method(f, t, y, h, n)
-	
-	generateGnuPlotConf(points, name)
-	
-	plot(name)
+    points = method(f, t, y, h, n)
+        
+    generateGnuPlotConf(points, name)
+        
+    plot(name)
 
-	print "\nHave a great day!"
+    print "\nHave a great day!"
 
-	return
+    return
 
 def runAllEulers():
-	
-	f = getFunction()
+        
+    f = getFunction()
 
-	t, y = getPoint(0)
+    t, y = getPoint(0)
 
-	h, n = getNandH()
+    h, n = getNandH()
 
-	points = eulers.euler(f, t, y, h, n)
-	points1 = eulers.euler_enhanced(f, t, y, h, n)
-	points2 = eulers.euler_inverted(f, t, y, h, n)
+    points = eulers.euler(f, t, y, h, n)
+    points1 = eulers.euler_enhanced(f, t, y, h, n)
+    points2 = eulers.euler_inverted(f, t, y, h, n)
 
-	generateGnuPlotConf(points, 'Euler')
-	plot('Euler')
-	generateGnuPlotConf(points1, "EulerEnhanced")
-	plot("EulerEnhanced")
-	generateGnuPlotConf(points2, "EulerInverted")
-	plot("EulerInverted")
+    generateGnuPlotConf(points, 'Euler')
+    plot('Euler')
+    generateGnuPlotConf(points1, "EulerEnhanced")
+    plot("EulerEnhanced")
+    generateGnuPlotConf(points2, "EulerInverted")
+    plot("EulerInverted")
 
-	print "\nHave a great day!"
+    print "\nHave a great day!"
 
-	return
+    return
 
-def runAdams(method, name, order):
-	f = getFunction()
-	
-	p = []
-	for i in range(order):
-		p.insert(0,Point2D(getPoint(i)))
-	
-	h, n = getNandH()
-	
-	points = method(f, p, h, n, order)
-	
-	generateGnuPlotConf(points, name)
-	plot(name)
-	
-	return
-	
-runAdams(adams_bashforth.method, 'Adams-bashforth', 6)
+def runAdamsBash(method, name, order):
+    f = getFunction()
+        
+    p = []
+    for i in range(order):
+        p.insert(0,Point2D(getPoint(i)))
+
+    h, n = getNandH()
+        
+    points = method(f, p, h, n, order)
+        
+    generateGnuPlotConf(points, name)
+    plot(name)
+        
+    return
+        
+def runAdamsMoulton(method, name, order):
+    f = getFunction()
+
+    p = []
+    print '\nObs: Adams-Moulton of order k needs only k-1 points\n'
+    for i in range(order - 1):
+        p.insert(0,Point2D(getPoint(i)))
+        
+    h, n = getNandH()
+        
+    points = method(f, p, h, n, order)
+        
+    generateGnuPlotConf(points, name)
+    plot(name)
+        
+    return
+
+runAdamsMoulton(adams_moulton.method, 'Adams-Moulton', 4)
