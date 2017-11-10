272f53a5b8c56cb185a1cf7873478ac6cba8293a
Nathan Prestwood <nmf2@cin.ufpe.br>
2017-11-09 23:40:54 -0300

    Final Commit

--- implementations/runge.py
+++ implementations/runge.py
@@ -1,22 +1,19 @@
 def method(f, t, y, h, n):
-	print "Running Runge-Kutta"
-	ts = [t]
-	ys = [y]
-	#points = ["{0} {1}\n".format(t, y)]  # save point as a string with the following format: "t y\n"
-	
-	for i in range(n):
-		k1 = f.subs('t',t).subs('y',y)
-		k2 = f.subs('t', t + h/2).subs('y',  k1 * h/2 + y)
-		k3 = f.subs('t', t + h / 2).subs('y', k2 * h / 2 + y)
-		k4 = f.subs('t', t + h ).subs('y', k3 * h + y)
-		
-		y = y + h*(k1 + 2*k2 + 2*k3 + k4)/6
-		t = t + h
-	
-		ts.append(t)
-		ys.append(y)
-		#points.append("{0} {1}\n".format(t,y))                  #put t in {0} and y in {1}
+    print "Running Runge-Kutta"
+    ts = [t]
+    ys = [y]
+    #points = ["{0} {1}\n".format(t, y)]  # save point as a string with the following format: "t y\n"
     
-        print "y(%.4f) = %.7f" % (ts[-1], y[-1])
-		
-	return ts, ys
+    for i in range(n):
+        k1 = f.subs('t',t).subs('y',y)
+        k2 = f.subs('t', t + h/2).subs('y',  k1 * h/2 + y)
+        k3 = f.subs('t', t + h / 2).subs('y', k2 * h / 2 + y)
+        k4 = f.subs('t', t + h ).subs('y', k3 * h + y)
+        
+        y = y + h*(k1 + 2*k2 + 2*k3 + k4)/6
+        t = t + h
+    
+        ts.append(t)
+        ys.append(y)
+        print "y(%.4f) = %.7f" % (ts[-1], ys[-1])
+    return ts, ys
